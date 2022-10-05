a08a2a8e88a3def29b31d1dea4df0a672d2d5609
haoyudong <2070343054@email.szu.edu.cn>
2022-10-06 00:05:11 +0800

    add pdf card in xrd

--- pages/xrd.py
+++ pages/xrd.py
@@ -10,6 +10,7 @@ datax=[]
 datay=[]
 file_name_arr=[]
 color_arr=[]
+child_upload_arr=[]
 def transform_txt(data):
   tempx = []
   tempy = []
@@ -27,8 +28,6 @@ def draw_xrd():
   for i in range(len(datax)):
     # 设置坐标轴名字
     axs[i].plot(datax[i],datay[i],linewidth=1,color=color_arr[i],label=file_name_arr[i])
-    # axs[i].set_xlabel('2 Theta',fontdict={'family':'Times New Roman','size':16})
-    # axs[i].set_ylabel('Intensity(a.u)',fontdict={'family':'Times New Roman','size':16})
     # 设置横坐标范围
     axs[i].set_xlim(10,80)
     # 设置横纵坐标刻度
@@ -44,11 +43,21 @@ def draw_xrd():
   fig.supxlabel(t='2 Theta',x=0.5,y=0.01,fontsize=16,family='Times New Roman',fontweight='bold')
   fig.supylabel(t='Intensity(a.u)',x=0.08,y=0.5,fontsize=16,family='Times New Roman',fontweight='bold')
   
-  return fig
+  return fig,axs
 def draw ():
   if uploaded_files:
-    f = draw_xrd()
-    st.pyplot(f)
+    fig,axs = draw_xrd()
+    # 遍历uploaded_files，画子图
+  for i,child_file in enumerate(child_upload_arr):
+    for child in child_file:
+      # 去掉后缀
+      child_name = re.sub(r'\.txt','',child.name)
+      # 读取文件
+      array= pd.read_table(child,sep="\t", header=None)
+      # 拿到x,y轴的数据放入数组中
+      tempx,tempy = transform_txt(array)
+      axs[i].scatter(tempx,tempy,linewidth=1,color='black',label=child_name,marker='*')
+  fig
 # 上传文件
 uploaded_files = st.file_uploader("上传txt文件", type="txt",accept_multiple_files=True,key='basic')
 # 文件上传完毕，推送到数组中
@@ -63,11 +72,11 @@ if uploaded_files:
     datax.append(tempx)
     datay.append(tempy)
     file_name_arr.append(file_name)
-  for i in range(len(datax)):
+  for i in range(len(file_name_arr)):
     # 配置
     st.sidebar.write(file_name_arr[i],"的配置文件")
     line_color = st.sidebar.color_picker('选择颜色', '#FF0000',key=file_name_arr[i])
     color_arr.append(line_color)
-    st.sidebar.file_uploader("上传标准PDF卡片文件", type="txt",accept_multiple_files=True,key=i)
-
+    upload = st.sidebar.file_uploader("上传标准PDF卡片文件", type="txt",accept_multiple_files=True,key=i)
+    child_upload_arr.append(upload)
 st.button("绘图",on_click=draw)

a4083a173ebc78cc3f8b449ff85faf4ad040b6b8
haoyudong <2070343054@email.szu.edu.cn>
2022-10-05 17:36:09 +0800

    generate mult xrd

--- pages/xrd.py
+++ pages/xrd.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from io import StringIO
 import streamlit as st
 import pandas as pd
-st.write("""
-# XRD数据分析
-需要上传两个txt文件:标准卡片和测试结果
-""")
+import re
+st.title("XRD数据分析")
+# 上传对应个数的文件
+uploaded_files = st.file_uploader("1.上传txt文件", type="txt",accept_multiple_files=True)
 def transform_txt(data):
   tempx = []
   tempy = []
@@ -20,20 +19,28 @@ def transform_txt(data):
   del tempx[0]
   del tempy[0]
   return tempx,tempy
-def draw_xrd(x,y):
+def draw_xrd(x,y,file_name,line_color):
   fig,ax = plt.subplots()
   my_x_ticks = np.arange(10,90,10)
-  plt.plot(tempx,tempy,linewidth=1,color='r',label='LIC-ZIF-67-M10')
+  plt.plot(x,y,linewidth=1,color=line_color,label=file_name)
   plt.xlabel('2 Theta',fontdict={'family':'Times New Roman','size':16})
   plt.ylabel('Intensity(a.u)',fontdict={'family':'Times New Roman','size':16})
   plt.xlim(10,80)
-  plt.xticks(my_x_ticks,fontproperties='Arial',size=14)
+  plt.xticks(my_x_ticks,fontproperties='Times New Roman',size=14)
   plt.yticks([])
   ax.legend(loc='upper right')
   st.pyplot(fig)
-uploaded_file = st.file_uploader("上传你测试生成的txt文件", type="txt")
-if uploaded_file is not None:
-    data= pd.read_table(uploaded_file,sep="\t", header=None)
-    tempx,tempy = transform_txt(data)
-    draw_xrd(tempx,tempy)
+# 根据上传文件数量生成对应个数的配置
+for uploaded_file in uploaded_files:
+    file_name_origin = uploaded_file.name
+    file_name = re.sub(r'\.txt','',file_name_origin)
+    st.write(file_name,"的XRD图例")
+    array= pd.read_table(uploaded_file,sep="\t", header=None)
+    tempx,tempy = transform_txt(array)
+    # 生成配置文件
+    st.sidebar.write(file_name,"的配置文件")
+    # 颜色
+    line_color = st.sidebar.color_picker('选择颜色', '#FF0000',key=file_name)
+    # 绘图
+    draw_xrd(tempx,tempy,file_name,line_color)
   
